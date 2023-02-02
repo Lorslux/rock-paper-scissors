cd109c4d91da2a24cc68189b7b943c47c9f5b5f7
Lorslux <lotavaresm@outlook.com>
2023-02-02 12:59:35 +0100

    funcionamiento basico player vs pc

---
+++ index.js
@@ -0,0 +1,71 @@
+const rockButton = document.getElementById("rock-button");
+const paperButton = document.getElementById("paper-button");
+const scissorsButton = document.getElementById("scissors-button");
+const cards = ["rock", "paper", "scissors"];
+let pcCard;
+let playerCard;
+
+rockButton.addEventListener("click", handleRockButtonClick);
+paperButton.addEventListener("click", handlePaperButtonClick);
+scissorsButton.addEventListener("click", handleScissorsButtonClick);
+
+function clearPlayerCard() {
+  const boxPlayer = document.getElementById("box-player");
+  boxPlayer.classList.remove("box-card-rock");
+  boxPlayer.classList.remove("box-card-paper");
+  boxPlayer.classList.remove("box-card-scissors");
+}
+
+function clearPcCard() {
+    const boxPc = document.getElementById("box-pc");
+    boxPc.classList.remove("box-card-rock");
+    boxPc.classList.remove("box-card-paper");
+    boxPc.classList.remove("box-card-scissors");
+  }
+
+function handleRockButtonClick() {
+  clearPlayerCard();
+  playPcAction();
+  document.getElementById("box-player").classList.add("box-card-rock");
+  playerCard = "rock";
+  checkResult();
+}
+
+function handlePaperButtonClick() {
+  clearPlayerCard();
+  playPcAction();
+  document.getElementById("box-player").classList.add("box-card-paper");
+  playerCard = "paper";
+  checkResult();
+}
+
+function handleScissorsButtonClick() {
+  clearPlayerCard();
+  playPcAction();
+  document.getElementById("box-player").classList.add("box-card-scissors");
+  playerCard = "scissors";
+  checkResult();
+}
+
+function getRandomChoice() {
+  return cards[Math.floor(Math.random() * 3)];
+}
+
+function playPcAction() {
+    clearPcCard();
+  const randomAction = getRandomChoice();
+  pcCard = randomAction;
+  const boxPc = document.getElementById("box-pc");
+  boxPc.classList.add(`box-card-${randomAction}`);
+}
+
+function checkResult(){
+    console.log("playerCard",playerCard, "pc card",pcCard);
+    if(playerCard === pcCard) {
+        setResult("it's a tie");
+    }
+}
+
+function setResult(result){
+    document.getElementById("result").innerHTML = result;
+}
