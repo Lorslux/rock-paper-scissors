e8def02e215457c5e53d2977b87442271417984d
Lorslux <lotavaresm@outlook.com>
2023-02-05 12:11:46 +0100

    pop-up instructions

--- index.js
+++ index.js
@@ -1,120 +1,13 @@
-const rockButton = document.getElementById("rock-button");
-const paperButton = document.getElementById("paper-button");
-const scissorsButton = document.getElementById("scissors-button");
-const resetButton = document.getElementById("reset-button");
-const cards = ["rock", "paper", "scissors"];
-let pcCard;
-let playerCard;
-let scorePlayer = 0;
-let scorePc = 0;
+const instructionsButton = document.getElementById("instructions-button");
+const closeInstructionsButton = document.getElementById("close-instructions");
 
-rockButton.addEventListener("click", handleRockButtonClick);
-paperButton.addEventListener("click", handlePaperButtonClick);
-scissorsButton.addEventListener("click", handleScissorsButtonClick);
-resetButton.addEventListener("click", resetGame);
+instructionsButton.addEventListener("click", handleInstructionsButton);
+closeInstructionsButton.addEventListener("click", handleCloseButton);
 
-function clearPlayerCard() {
-  const boxPlayer = document.getElementById("box-player");
-  boxPlayer.classList.remove("box-card-rock");
-  boxPlayer.classList.remove("box-card-paper");
-  boxPlayer.classList.remove("box-card-scissors");
+function handleInstructionsButton() {
+  document.body.classList.add("show-instructions");
 }
 
-function clearPcCard() {
-  const boxPc = document.getElementById("box-pc");
-  boxPc.classList.remove("box-card-rock");
-  boxPc.classList.remove("box-card-paper");
-  boxPc.classList.remove("box-card-scissors");
-}
-
-function handleRockButtonClick() {
-  clearPlayerSelectedCard();
-  this.classList.add("selected");
-
-  clearPlayerCard();
-  playPcAction();
-  document.getElementById("box-player").classList.add("box-card-rock");
-  playerCard = "rock";
-  checkResult();
-}
-
-function handlePaperButtonClick() {
-  clearPlayerSelectedCard();
-  this.classList.add("selected");
-  clearPlayerCard();
-  playPcAction();
-  document.getElementById("box-player").classList.add("box-card-paper");
-  playerCard = "paper";
-  checkResult();
-}
-
-function handleScissorsButtonClick() {
-  clearPlayerSelectedCard();
-  this.classList.add("selected");
-  clearPlayerCard();
-  playPcAction();
-  document.getElementById("box-player").classList.add("box-card-scissors");
-  playerCard = "scissors";
-  checkResult();
-}
-
-function getRandomChoice() {
-  return cards[Math.floor(Math.random() * 3)];
-}
-
-function playPcAction() {
-  clearPcCard();
-  const randomAction = getRandomChoice();
-  pcCard = randomAction;
-  const boxPc = document.getElementById("box-pc");
-  boxPc.classList.add(`box-card-${randomAction}`);
-}
-
-function checkResult() {
-  console.log("playerCard", playerCard, "pc card", pcCard);
-  if (playerCard === pcCard) {
-    setResult("it's a tie");
-    return;
-  }
-  if (
-    (playerCard === "rock" && pcCard === "scissors") ||
-    (playerCard === "scissors" && pcCard === "paper") ||
-    (playerCard === "paper" && pcCard === "rock")
-  ) {
-    scorePlayer++;
-    setResult("🚩 YOU win! 🚩");
-  } else {
-    scorePc++;
-    setResult("PC wins!");
-  }
-}
-
-function setResult(result) {
-  document.getElementById("result").innerHTML = result;
-  updateScoreOnScreen();
-}
-
-function clearPlayerSelectedCard() {
-  document.getElementById("rock-button").classList.remove("selected");
-  document.getElementById("paper-button").classList.remove("selected");
-  document.getElementById("scissors-button").classList.remove("selected");
-}
-
-function updateScoreOnScreen() {
-  document.getElementById("player-score").innerHTML = scorePlayer;
-  document.getElementById("pc-score").innerHTML = scorePc;
-}
-
-function resetScore() {
-  scorePlayer = 0;
-  scorePc = 0;
-  updateScoreOnScreen();
-}
-
-function resetGame() {
-  resetScore();
-  setResult("Play Your Move!");
-  clearPlayerSelectedCard();
-  clearPlayerCard();
-  clearPcCard();
+function handleCloseButton() {
+  document.body.classList.remove("show-instructions");
 }
